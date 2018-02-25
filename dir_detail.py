61abad63309c9fc1c71dc21b5646c4a325bedeef
Rockheung <rockheung@gmail.com>
2018-02-25 19:14:20 +0900

    init

---
+++ dir_detail.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import os
+ttyR, ttyC =map( int, os.popen( 'stty size', 'r').read().split() )
+
+
+def d( dictStyle =eval( 'globals()'), kwd ='', exc =[ '__doc__'] ):
+
+  dictKeys = dictStyle.keys()
+  maxLen =max( map( len, dictKeys ) )
+
+  try :
+    dictKeys =sorted( dictKeys)
+  except TypeError:
+    pass
+
+  for i in dictKeys:
+    if i not in exc:
+      try :
+        print('├ {:^{w}}│ {}'.format( i, dictStyle[i], w =maxLen ) )
+      except TypeError:
+        print('├ {}│ {}'.format( i, dictStyle[i] ) )
+
+
+
+def dd( obj =None, kwd ='', exc =[ '__doc__'] ):
+  objDefault ='__main__' not in dir() and obj is None
+
+  if objDefault :
+    import __main__
+    obj =__main__
+
+  mList =dir( obj)
+  maxLen =max( map( len, mList ) )
+  for mI in mList:
+    try:
+      mExplain =eval( obj.__name__ +'.' +mI )
+      if type( mExplain) is dict:
+        print( mI)
+        d(mExplain)
+      else :
+        print( '{:^{w}}│ {}'.format( mI, mExplain, w =maxLen ) )
+
+    except AttributeError:
+      print( '{:^{w}}'.format( mI, w =maxLen ) )
+
+    except NameError:
+      print( '{}'.format( mI) )
+
+  if objDefault :
+    del __main__
+
+def ddd( obj =None, kwd ='', exc =[ '__doc__'] ):
+  objDefault ='__main__' not in dir() and obj is None
+
+  if objDefault :
+    import __main__
+    obj =__main__
+
+  
+
+
+  if objDefault :
+    del __main__
