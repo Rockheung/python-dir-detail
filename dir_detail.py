0c23003cd8cd07b5cca66dd47361d71cc60fc6dc
Rockheung <rockheung@gmail.com>
2018-02-28 02:47:00 +0900

    0.1

--- dir_detail.py
+++ dir_detail.py
@@ -1,89 +1,89 @@
 import os
 
-def d( kwd ='', obj =globals(), exc =[ '__doc__'], lines =1 ):
-  ttyw =int( os.popen( 'stty size', 'r').read().split()[1] )
+def d( kwd ='', obj =globals(), root ='/', esc =[ '__doc__']):
 
-  objKeys =obj.keys()
-  maxLen =max( map( len, objKeys ) )
+  # Get terminal's pixel width from os
+  ttyh, ttyw =tuple( list( map( int, os.popen( 'stty size', 'r').read().split() ) ) )
 
-  try :
-    objKeys =list( filter( lambda x: kwd in x, objKeys ) )
-    objKeys =sorted( objKeys)
-
-  except TypeError:
-    objEtcs =list( filter( lambda x: type( x) is str, objKeys ) )
-    objKeys =list( filter( lambda x: x not in objEtcs, objKeys ) )
-    objKeys =list( filter( lambda x: kwd in x, objKeys ) )
-    objKeys =sorted( objKeys)
-
-
+  # Print table from dict type data including string type kwd variable
   while True:
 
-    for i in objKeys:
-      if i not in exc:
-        print('{:^{w}}| {:.{p}}'.format( i, str(obj[i]), w =maxLen, p =ttyw -maxLen -2 ) )
+    # Sort keys by alphabetical order.
+    # If Non-string type key exist, exclude it.
+    try :
+      objKeys =list( filter( lambda x: kwd in x, obj.keys() ) )
+      objKeys =sorted( objKeys)
 
-    kwd =input('\\ ')
+    except TypeError:
+      objEtcs =list( filter( lambda x: type( x) is str, obj.keys() ) )
+      objKeys =list( filter( lambda x: x not in objEtcs, obj.keys() ) )
+      objKeys =list( filter( lambda x: kwd in x, objKeys ) )
+      objKeys =sorted( objKeys)
 
-    if kwd == '' or len(objKeys) ==0 :
+    # Get key's max string length.
+    maxLen =max( map( len, objKeys ) )
+
+    largerTen = 2 if len( objKeys) >=100 else 1 if len( objKeys) >=10 else 0
+
+    try :
+      assert largerTen <=2
+    except AssertionError :
+      print( 'Too big dictionary')
       break
-    elif len(objKeys) ==1 :
-      objKey = objKeys
 
-      objKeys =dir( eval( objKey) )
+    for i, key in enumerate(objKeys, start =1) :
+      explain =str(obj[key]) if '\n' not in str(obj[key]) else str(obj[key]).split('\n')[0] +' ...More'
+      print('{:{n}d}.{:^{w}}| {:.{p}}'.format( i,
+                                               key,
+                                               explain,
+                                               n =1 +largerTen,
+                                               w =maxLen,
+                                               p =ttyw -maxLen -4 -largerTen )
+           )
 
-    objKeys =list( filter( lambda x: kwd in x, objKeys ) )
+    kwd =input('{} '.format( root) )
+    objKeys =list( filter( lambda x: kwd[:-1] if kwd[-1] =='/' else kwd in x, objKeys ) )
 
-  
+    if kwd =='..' :
+      return
 
+    elif kwd =='' :
+      break
 
+    elif kwd =='.' :
+      kwd =''
+      continue
 
-# def ddir( obj =object, kwd ='', exc =[ '__doc__'] ):
-#   isobjDefault ='__main__' not in dir() and obj is object
-# 
-#   if isobjDefault :
-#     import __main__
-#     obj =__main__
-# 
-#   print( repr( obj) )
-# 
-#   try :
-#     isClass =issubclass( obj, object)
-#   except TypeError :
-#     isClass =False
-# 
-#   moduleList =dir( obj)
-#   maxLen =max( map( len, moduleList ) )
-# 
-#   try :
-#     moduleName =obj.__name__
-# 
-#   except AttributeError:
-#     try :
-#       moduleName =repr( obj).split("'")[1]
-# 
-#     except IndexError:
-#       print( 'Failed to get module name. Check it out.')
-#       return
-# 
-#   isDunbar =obj.__name__ is not repr( obj).split("'")[1]
-#   moduleName =obj.__name__ if not isDunbar and not isClass else repr( obj).split("'")[1]
-# 
-#   for mI in moduleList:
-#     try :
-#       mExplain =repr( eval( moduleName +'.' +mI ) )
-# 
-#     except AttributeError:
-#       print( '{:^{w}}'.format( mI, w =maxLen ) )
-#       break
-# 
-#     if type( mExplain) is obj:
-#       print( mI)
-#       d(mExplain)
-# 
-#     else :
-#       print( '{:^{w}}| {}'.format( mI, mExplain, w =maxLen ) )
-# 
-#   if isobjDefault :
-#     del __main__
-# 
+    elif len( objKeys ) ==1 :
+
+      new_obj =dict()
+      subroot ='{}{}'.format( '.'.join( root.split('/')[1:] ),
+                                objKeys[0] )
+
+      for i in dir( eval( subroot ) ):
+        try :
+          new_obj[i] =eval( subroot +'.' +i )
+        except AttributeError :
+          new_obj[i] =None
+
+      d( obj =new_obj, root =root +objKeys[0] +'/' )
+      kwd =''
+
+    elif kwd[-1] =='/' and kwd[:-1] in objKeys :
+
+      new_obj =dict()
+      subroot = '{}{}'.format( '.'.join( root.split('/')[1:] ),
+                                 kwd[:-1] )
+
+      print(subroot, '\n')
+      for i in dir( eval( subroot ) ):
+        try :
+          new_obj[i] =eval( subroot +'.' +i )
+        except AttributeError :
+          new_obj[i] =None
+
+      d( obj =new_obj, root =root +kwd )
+      kwd =''
+
+    else :
+      pass
